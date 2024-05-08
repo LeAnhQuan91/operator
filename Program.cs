e1ade914ac44570ba1307949939e35c3f155694a
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-05-08 19:42:46 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        float width;
+        float heigth;
+
+        Console.WriteLine("Nhap vao so width: ");
+       width = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap vao so heigth");
+        heigth = float.Parse(Console.ReadLine());
+
+        float area = width * heigth;
+
+        Console.WriteLine($"Chieu rong la: {width}, Chieu cao la: {heigth},Dien tich hinh chu nhat la: {width}*{heigth}={area} ");
+    }
+}
